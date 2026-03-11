9a238d5f9bfbf93abaaddac3fa8b4e24e8b71860
Jake <justinejake16@gmail.com>
2026-03-11 23:50:42 +0800

    project creation and checking

---
+++ programs_batch_1/quotient_with_decimal_points.py
@@ -0,0 +1,6 @@
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+
+quotient = first_number / second_number
+
+print(f"The quotient is: {quotient}")

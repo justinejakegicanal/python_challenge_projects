5b0390b413694a75a642cba447a72aa91d9ab14d
Jake <justinejake16@gmail.com>
2026-03-11 23:26:40 +0800

    program creation and checking

---
+++ programs_batch_1/find_bigger_number.py
@@ -0,0 +1,7 @@
+first_number = float(input("First number: "))
+second_number = float(input("Second number: "))
+
+if first_number > second_number:
+    print(f"The bigger number is: {first_number}")
+else:
+    print(f"The bigger number is: {second_number}")

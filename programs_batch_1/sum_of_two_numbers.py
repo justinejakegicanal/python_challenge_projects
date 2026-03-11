5e224fa3217ebb0d51add6a9e1ecb5192c95cf83
Jake <justinejake16@gmail.com>
2026-03-11 23:41:56 +0800

    project creation and checking

---
+++ programs_batch_1/sum_of_two_numbers.py
@@ -0,0 +1,6 @@
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+
+sum = first_number + second_number
+
+print(f"The sum of two numbers are: {sum}")     

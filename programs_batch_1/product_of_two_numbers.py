5b0b0a30b51000138632106a6a041b3d73c0579a
Jake <justinejake16@gmail.com>
2026-03-11 23:47:11 +0800

    project creation and checking

---
+++ programs_batch_1/product_of_two_numbers.py
@@ -0,0 +1,6 @@
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+
+product = first_number * second_number
+
+print(f"The product is: {product}")

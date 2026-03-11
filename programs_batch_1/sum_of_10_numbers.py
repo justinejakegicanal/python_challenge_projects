106aad3d6abcdd40edec52d9f533d2dd20d3ae32
Jake <justinejake16@gmail.com>
2026-03-12 00:07:18 +0800

    project creation and checking

---
+++ programs_batch_1/sum_of_10_numbers.py
@@ -0,0 +1,7 @@
+collected_numbers = 0
+
+for i in range(10):
+    number = int(input("Enter a number: "))
+    collected_numbers += number 
+
+print(f"The sum of the 10 numbers is: {collected_numbers}")

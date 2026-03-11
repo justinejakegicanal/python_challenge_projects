bfdea12526ee0533594822f477cd07db70161130
Jake <justinejake16@gmail.com>
2026-03-12 00:00:52 +0800

    project creation and checking

---
+++ programs_batch_1/raise_to_the_power.py
@@ -0,0 +1,6 @@
+base_number = float(input("Enter base number: "))
+exponent_number = float(input("Enter exponent number: "))
+
+exponentation = base_number ** exponent_number
+
+print(f"The result is: {exponentation}")

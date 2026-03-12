53f490a9443947c552d3ceb6fafd7847276ac33d
Jake <justinejake16@gmail.com>
2026-03-12 08:40:54 +0800

    Project creation and testing

---
+++ programs_batch_2/not_equal_when_numbers_not_same.py
@@ -0,0 +1,8 @@
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))   
+
+if first_number != second_number:
+    print("Not Equal")
+else:
+    print("Equal")
+    

040a27bf93af22721d313145b6c57127e92080b2
Jake <justinejake16@gmail.com>
2026-03-12 08:27:17 +0800

    Project creation and checking

---
+++ programs_batch_2/find_smaller_number.py
@@ -0,0 +1,7 @@
+first_number = float(input("First number: "))
+second_number = float(input("Second number: "))
+
+if first_number > second_number:
+    print(f"The smaller number is: {second_number}")
+else:
+    print(f"The smaller number is: {first_number}")

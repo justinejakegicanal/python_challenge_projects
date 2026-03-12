b1c545e22f7d20d0b70c3aacffc1bbe335c872c8
Jake <justinejake16@gmail.com>
2026-03-12 10:02:33 +0800

    Project creation and checking

---
+++ programs_batch_2/numbers_between_first_and_second_number.py
@@ -0,0 +1,6 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+                    
+for number in range(first_number + 1, second_number):
+    print(number)
+    
